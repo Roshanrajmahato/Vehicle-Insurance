7929e101115b0b4860dba14508f503c233f98940
Roshanrajmahato <roshanrajmahato9204@gmail.com>
2025-04-27 13:40:17 +0530

    project structure is done and virtual environment is set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup ,find_packages
+
+setup(
+    name="src",
+    version="0.0.2",
+    author="Roshan Raj Mahato",
+    author_email="roshanrajmahato9204@gmail.com",
+    packages=find_packages()
+)
